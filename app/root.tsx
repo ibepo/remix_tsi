79ae208068ac2f46671c1ce5461e0d1704c8a25f
ibepo <kefanbufan@163.com>
2024-04-28 16:36:27 +0800

    完成首页分页显示 和添加页面 以及基于remix框架的基础搭建

--- app/root.tsx
+++ app/root.tsx
@@ -1,29 +1,27 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { NextUIProvider } from '@nextui-org/react'
+import type { LinksFunction } from '@remix-run/node'
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
+import stylesheet from 'app/main.css?url'
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
       <body>
-        {children}
+        <NextUIProvider>
+          <Outlet />
+        </NextUIProvider>
         <ScrollRestoration />
         <Scripts />
+        <LiveReload />
       </body>
     </html>
-  );
+  )
 }
 
-export default function App() {
-  return <Outlet />;
-}
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: stylesheet }]
